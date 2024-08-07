fa77da61571da1bfeace9ed4eb6d54a5a90b2fdd
DaniSanchez0707 <danisanc927@gmail.com>
2024-08-06 21:46:40 -0600

    deploy3

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import app from './app.js';
 
 
  dotenv.config()
-
+console.log(process.env.DB_HOST)
  const port = process.env.NODE_PORT
 app.listen(port, () => {
     console.log('listening on port ', port);
